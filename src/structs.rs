f7503ddf0c5deca14b3ba1ddd3ff44624cdd71b4
TTsdzb <ttsdzb@outlook.com>
2024-12-11 16:10:53 +0800

    feat(structs.rs): convert array or tuple to points
    Convert tuple or array to points.

--- src/structs.rs
+++ src/structs.rs
@@ -10,4 +10,12 @@ impl Point {
     pub fn new(x: u32, y: u32) -> Self {
         Self { x, y }
     }
+
+    pub fn from_tuple(p: &(u32, u32)) -> Self {
+        Self { x: p.0, y: p.1 }
+    }
+
+    pub fn from_array(p: &[u32; 2]) -> Self {
+        Self { x: p[0], y: p[1] }
+    }
 }
