1a6c53c50e283e45d45d16de5fa1742b72c98588
TTsdzb <ttsdzb@outlook.com>
2025-03-15 11:35:31 +0800

    fix(adb_commands): adb screenshot on windows
    Could not decode images from adb on Windows.

--- src/adb_commands.rs
+++ src/adb_commands.rs
@@ -6,6 +6,28 @@ use image::{DynamicImage, ImageError, ImageReader};
 use std::io::Cursor;
 use std::{io, process::Command};
 
+#[cfg(target_os = "windows")]
+fn replace_crlf(buffer: Vec<u8>) -> Vec<u8> {
+    let mut iter = buffer.into_iter().peekable();
+    let mut result = Vec::new();
+
+    while let Some(current) = iter.next() {
+        if current == b'\r' {
+            if let Some(&next) = iter.peek() {
+                if next == b'\n' {
+                    result.push(b'\n');
+                    iter.next();
+                    continue;
+                }
+            }
+            result.push(current);
+        } else {
+            result.push(current);
+        }
+    }
+    result
+}
+
 #[derive(Error, Debug)]
 pub enum AdbScreenshotError {
     #[error("Adb screenshot command invoke failed: {0}")]
@@ -42,7 +64,12 @@ pub fn screenshot() -> Result<DynamicImage, AdbScreenshotError> {
         });
     }
 
-    Ok(ImageReader::new(Cursor::new(output.stdout))
+    #[cfg(target_os = "windows")]
+    let image_data = replace_crlf(output.stdout);
+    #[cfg(not(target_os = "windows"))]
+    let image_data = output.stdout;
+
+    Ok(ImageReader::new(Cursor::new(image_data))
         .with_guessed_format()?
         .decode()?)
 }
